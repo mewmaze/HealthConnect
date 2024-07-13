71e3d87fcc7f8fed156333a8d9bc6405ec267188
mewmaze <mewmaze@naver.com>
2024-07-13 14:11:37 +0900

    img 보이게 추가하고 서버연결코드로 수정

--- src/Project/components/ChallengeItem.js
+++ src/Project/components/ChallengeItem.js
@@ -1,17 +1,20 @@
 import { useNavigate } from 'react-router-dom';
 import './ChallengeItem.css';
 
-function ChallengeItem({id,challengename,participantCount}){
+function ChallengeItem({challenge_id,challenge_img,challenge_name,participant_count}){
     const navigate = useNavigate();
 
     const goDetail = () => {
-        navigate(`/challengeDetail/${id}`);
+        navigate(`/challengeDetail/${challenge_id}`);
     }
 
     return(
         <div className="ChallengeItem" onClick={goDetail}>
-            <div className="challengeItem-name">{challengename}</div>
-            <div className="challengeItem-participant">{participantCount}명 </div>
+            <div className='ChallengeItem-img'>
+                <img src={`http://localhost:5000/${challenge_img}`} alt={challenge_name}/>
+            </div>
+            <div className="challengeItem-name">{challenge_name}</div>
+            <div className="challengeItem-participant">{participant_count}명 도전중 </div>
         </div>
     )
 }
