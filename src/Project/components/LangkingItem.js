23aa65e445564881eb0d722d35fbd80f1ac8833a
mewmaze <mewmaze@naver.com>
2024-07-22 18:01:07 +0900

    LangkingList 수정

--- src/Project/components/LangkingItem.js
+++
@@ -1,9 +0,0 @@
-function LangkingItem(){
-    return(
-        <div className="LangkingItem">
-            <img src="/emotion1.png" alt="프로필사진"/>
-            <div className="langking_nickname">닉네임</div>
-        </div>
-    )
-}
-export default LangkingItem
--- src/Project/components/LangkingList.js
+++ src/Project/components/LangkingList.js
@@ -1,13 +1,24 @@
-import "./LangkingList.css";
+// import { Link } from 'react-router-dom';
+// import "./LangkingList.css";
 
-function LangkingList (){
-    return(
-        <div className="LangkingList">
-            <div className="LangkingBox">
-                <img src="/emotion1.png" alt="프로필사진"/>
-                <div className="langking_nickname">닉네임</div>
-            </div>
-        </div>
-    )
-}
-export default LangkingList;
+// function LangkingList ({username,profile_picture}){
+
+//     return(
+//         <div className="LangkingList">
+//             {/* <Link to={} className="LangkingItem"> */}
+//                 <div className="Langking-img">
+//                     {/* <img src={`http://localhost:5000/${profile_pictureg}`} alt="프로필사진"/> */}
+//                     {data.map((it)=>(
+//                         key={it.user_id} {username}
+//                     ))}                    
+
+//                 </div>                
+//             {/* </Link> */}
+//         </div>
+//     )
+// }
+// export default LangkingList;
+
+// {data.map((it)=>(
+//     <ChallengeItem key={it.challenge_id} {...it} />
+// ))}
