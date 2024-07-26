e3c7db7ae0aad1d6b407464e317c25bc9161ae95
mewmaze <mewmaze@naver.com>
2024-07-26 14:15:53 +0900

    글 목록이랑 챌린지 목록 메인페이지에 불러오게 수정

--- src/Project/pages/Home.js
+++ src/Project/pages/Home.js
@@ -1,11 +1,57 @@
 //챌린지랑 운동기록페이지로 이동하는 임시 페이지
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
 import "./Home.css";
 
 function Home(){
+    const [challenges, setChallenges] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        const fetchChallenges = async () => {
+            try {
+                const [challengesResponse, postsResponse] = await Promise.all([
+                    axios.get('http://localhost:5000/challenges'), // 챌린지 데이터 불러오기
+                    axios.get('http://localhost:5000/posts') // 게시글 데이터 불러오기
+                ]);
+                setChallenges(challengesResponse.data);
+                setPosts(postsResponse.data);
+            } catch (error) {
+                setError('Failed to fetch challenges.');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchChallenges();
+    }, []);
     return(
-        <div>
-            <div className="title">메인 페이지 입니다.</div>
+        <div className="Home">
+            {loading && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            <input className="HomeSearch" placeholder="검색어를 입력하세요"></input>
+            <div>인기있는 챌린지</div>
+            <ul>
+                {challenges.map(challenge => (
+                    <li key={challenge.challenge_id}>
+                    <Link to={`/challengeDetail/${challenge.challenge_id}`}>
+                        <div>{challenge.challenge_name}</div>
+                    </Link>
+                </li>
+                ))}
+            </ul>
+            <div>커뮤니티</div>
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <Link to={`/postDetail/${post.id}`}>
+                            <div>{post.title}</div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
 
     )
