423dd033b42afe452e2f6569c9509fc82f54c400
mewmaze <mewmaze@naver.com>
2024-07-26 14:16:57 +0900

    수정

--- src/Project/App.js
+++ src/Project/App.js
@@ -177,6 +177,7 @@ const App = () => {
                 <Route path="/community/:communityId" element={<Community communities={communities} />} />
                 <Route path="/community/:communityId/new-post" element={<NewPostPage addPost={addPost} />} />
                 <Route path="/community/:communityId/post/:postId" element={<PostDetail communities={communities} addComment={addComment} />} />
+                <Route path="/postDetail/:postId" element={<PostDetail />} />
               </Routes>
               </Layout>
             </div>
