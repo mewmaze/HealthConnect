c478126a51022a48a9a10f5e768a55a77a2d874b
mewmaze <mewmaze@naver.com>
2024-08-02 07:11:47 +0900

    수정

--- src/Project/components/ExerciseSet.js
+++ src/Project/components/ExerciseSet.js
@@ -5,11 +5,10 @@ import './ExerciseSet.css';
 
 function ExerciseSet() {
     const { currentUser } = useContext(AuthContext); 
-
     const userId = currentUser?.user_id;
 
     const [basicInfo, setBasicInfo] = useState({ height: '', weight: '', bmi: '' });
-    const [goal, setGoal] = useState({ height: '', weight: '', bmi: '' });
+    const [goal, setGoal] = useState({ goal_height: '', goal_weight: '', goal_bmi: '' });
     const [editing, setEditing] = useState(false);
 
     useEffect(() => {
@@ -17,8 +16,16 @@ function ExerciseSet() {
             try {
                 if (userId) {
                     const response = await axios.get(`http://localhost:5000/user/${userId}`);
-                    setBasicInfo(response.data);
-                    setGoal(response.data);
+                    setBasicInfo({
+                        height: response.data.height,
+                        weight: response.data.weight,
+                        bmi: response.data.bmi
+                    });
+                    setGoal({
+                        goal_height: response.data.goal_height,
+                        goal_weight: response.data.goal_weight,
+                        goal_bmi: response.data.goal_bmi
+                    });
                 }
             } catch (error) {
                 console.error('Failed to fetch user basicInfo or Goal:', error);
@@ -39,94 +46,112 @@ function ExerciseSet() {
 
     const handleSave = async () => {
         try {
-            await axios.put(`http://localhost:5000/user/${userId}`, {
-                ...basicInfo,
-                ...goal
+            console.log('Sending data:', {
+                basicInfo,  // 현재 상태 정보
+                goal        // 목표 정보
             });
+
+            const response = await axios.put(`http://localhost:5000/user/${userId}`, {
+                height: basicInfo.height,
+                weight: basicInfo.weight,
+                bmi: basicInfo.bmi,
+                goal_height: goal.goal_height,
+                goal_weight: goal.goal_weight,
+                goal_bmi: goal.goal_bmi
+            });
+            console.log('Server response:', response.data);
+
             setEditing(false);
-            alert('정보가 성공적으로 업데이트되었습니다.');
+
 
         } catch (error) {
             console.error('Failed to update user information:', error);
-            alert('정보 업데이트에 실패했습니다.');
+
+        }
+    };
+
+    const handleEditToggle = () => {
+        if (editing) {
+            handleSave();  // 저장할 때 서버에 업데이트 요청
+        } else {
+            setEditing(true);
         }
     };
 
     return (
-            <div className="exerciseSet">
-                <div className='exerciseSet-Top'>
-                    <div className='Now'>
-                        <div className='exerciseSet-title'>현재 나의 상태</div>
-                        {editing ? (
-                            <div>
-                                <input 
-                                    type="text" 
-                                    name="height" 
-                                    value={basicInfo.height} 
-                                    onChange={(e) => handleInputChange(e, 'basic')} 
-                                />
-                                <input 
-                                    type="text" 
-                                    name="weight" 
-                                    value={basicInfo.weight} 
-                                    onChange={(e) => handleInputChange(e, 'basic')} 
-                                />
-                                <input 
-                                    type="text" 
-                                    name="bmi" 
-                                    value={basicInfo.bmi} 
-                                    onChange={(e) => handleInputChange(e, 'basic')} 
-                                />
-                            </div>
-                        ) : (
-                            <div>
-                                <p>키 : {basicInfo.height || '키를 입력해 주세요'}</p>
-                                <p>몸무게 : {basicInfo.weight || '체중을 입력해 주세요'}</p>
-                                <p>BMI : {basicInfo.bmi || 'BMI를 입력해 주세요'}</p>
-                            </div>
-                        )}
-                    </div>
-                    <div className='Goal'>
-                        <div className='exerciseSet-title'>목표</div>
-                        {editing ? (
-                            <div>
-                                <input 
-                                    type="text" 
-                                    name="height" 
-                                    value={goal.height} 
-
-                                    placeholder='목표 키를 입력해주세요'
-                                    onChange={(e) => handleInputChange(e, 'goal')} 
-                                />
-                                <input 
-                                    type="text" 
-                                    name="weight" 
-                                    value={goal.weight} 
-                                    placeholder='목표 체중을 입력해주세요'
-                                    onChange={(e) => handleInputChange(e, 'goal')} 
-                                />
-                                <input 
-                                    type="text" 
-                                    name="bmi" 
-                                    value={goal.bmi} 
-                                    placeholder='목표 BMI를 입력해주세요'
-                                    onChange={(e) => handleInputChange(e, 'goal')} 
-                                />
-                            </div>
-                        ) : (
-                            <div>
-                                <p>키 : {goal.height || '목표 키를 설정해 주세요'}</p>
-                                <p>몸무게 : {goal.weight || '목표 몸무게를 설정해 주세요'}</p>
-                                <p>BMI : {goal.bmi || '목표 BMI를 설정해 주세요'}</p>
-                            </div>
-                        )}
-                    </div>
+        <div className="exerciseSet">
+            <div className='exerciseSet-Top'>
+                <div className='Now'>
+                    <div className='exerciseSet-title'>현재 나의 상태</div>
+                    {editing ? (
+                        <div>
+                            <input 
+                                type="text" 
+                                name="height" 
+                                value={basicInfo.height} 
+                                onChange={(e) => handleInputChange(e, 'basic')} 
+                            />
+                            <input 
+                                type="text" 
+                                name="weight" 
+                                value={basicInfo.weight} 
+                                onChange={(e) => handleInputChange(e, 'basic')} 
+                            />
+                            <input 
+                                type="text" 
+                                name="bmi" 
+                                value={basicInfo.bmi} 
+                                onChange={(e) => handleInputChange(e, 'basic')} 
+                            />
+                        </div>
+                    ) : (
+                        <div>
+                            <p>키 : {basicInfo.height || '키를 입력해 주세요'}</p>
+                            <p>몸무게 : {basicInfo.weight || '체중을 입력해 주세요'}</p>
+                            <p>BMI : {basicInfo.bmi || 'BMI를 입력해 주세요'}</p>
+                        </div>
+                    )}
                 </div>
-
-                <button className='exerciseSet-btn' onClick={() => setEditing(!editing)}>
-                    {editing ? '저장' : '수정'}
-                </button>
+                <div className='Goal'>
+                    <div className='exerciseSet-title'>목표</div>
+                    {editing ? (
+                        <div>
+                            <input 
+                                type="text" 
+                                name="goal_height" 
+                                value={goal.goal_height} 
+                                placeholder='목표 키를 입력해주세요'
+                                onChange={(e) => handleInputChange(e, 'goal')} 
+                            />
+                            <input 
+                                type="text" 
+                                name="goal_weight" 
+                                value={goal.goal_weight} 
+                                placeholder='목표 체중을 입력해주세요'
+                                onChange={(e) => handleInputChange(e, 'goal')} 
+                            />
+                            <input 
+                                type="text" 
+                                name="goal_bmi" 
+                                value={goal.goal_bmi} 
+                                placeholder='목표 BMI를 입력해주세요'
+                                onChange={(e) => handleInputChange(e, 'goal')} 
+                            />
+                        </div>
+                    ) : (
+                        <div>
+                            <p>키 : {goal.goal_height || '목표 키를 설정해 주세요'}</p>
+                            <p>몸무게 : {goal.goal_weight || '목표 몸무게를 설정해 주세요'}</p>
+                            <p>BMI : {goal.goal_bmi || '목표 BMI를 설정해 주세요'}</p>
+                        </div>
+                    )}
+                </div>
             </div>
+
+            <button className='exerciseSet-btn' onClick={handleEditToggle}>
+                {editing ? '저장' : '수정'}
+            </button>
+        </div>
     );
 }
 
