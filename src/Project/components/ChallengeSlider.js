4d1afd7b30a91ae292d498372bc918b80730e395
mewmaze <mewmaze@naver.com>
2025-04-06 00:32:26 +0900

    fix: api 호출 변경

--- src/Project/components/ChallengeSlider.js
+++ src/Project/components/ChallengeSlider.js
@@ -1,69 +1,67 @@
-import { useEffect, useState } from 'react';
-import Slider from 'react-slick';
-import ChallengeItem from './ChallengeItem';
-import './ChallengeSlider.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import ChallengeItem from "./ChallengeItem";
+import "./ChallengeSlider.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ChallengeSlider = () => {
-    const [challenges, setChallenges] = useState([]);
-
-    useEffect(() => {
-        const fetchChallenges = async () => {
-            try {
-                const response = await fetch('http://localhost:5000/challenges');
-                const data = await response.json();
-                setChallenges(data.slice(0, 8));
-            } catch (error) {
-                console.error('Failed to fetch challenges:', error);
-            }
-        };
-
-        fetchChallenges();
-    }, []);
-
-
-    const settings = {
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        autoplay: false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
+  const [challenges, setChallenges] = useState([]);
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+  useEffect(() => {
+    const fetchChallenges = async () => {
+      try {
+        const response = await fetch(`${API_BASE_URL}/challenges`);
+        const data = await response.json();
+        setChallenges(data.slice(0, 8));
+      } catch (error) {
+        console.error("Failed to fetch challenges:", error);
+      }
     };
-    return (
-        <div className='ChallengeSlider'>
-            <Slider {...settings}>
-                {challenges.slice(0,8).map((item) => (
-                    <ChallengeItem key={item.challenge_id} {...item}/>
-                ))}
-            </Slider>
-        </div>
-    )
+
+    fetchChallenges();
+  }, []);
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="ChallengeSlider">
+      <Slider {...settings}>
+        {challenges.slice(0, 8).map((item) => (
+          <ChallengeItem key={item.challenge_id} {...item} />
+        ))}
+      </Slider>
+    </div>
+  );
 };
 export default ChallengeSlider;
