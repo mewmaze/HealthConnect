d649c63e15d1ddfa52c08d2c6b3c34032994aaf3
mewmaze <mewmaze@naver.com>
2025-09-16 20:55:16 +0900

    feature: 파일경로 추가가

--- src/hooks/useNavigation.js
+++ src/hooks/useNavigation.js
@@ -4,9 +4,19 @@ export const useNavigation = () => {
   const navigate = useNavigate();
 
   const goHome = () => navigate("/");
+  const goChallenge = () => navigate("/challenge");
+  const goCommunity = () => navigate("/communities");
   const goExercise = () => navigate("/exercise");
   const goLogin = () => navigate("/login");
   const goSignup = () => navigate("/signup");
   const goMyPage = (user_id) => navigate(`/myPage/${user_id}`);
-  return { goHome, goExercise, goLogin, goSignup, goMyPage };
+  return {
+    goHome,
+    goChallenge,
+    goCommunity,
+    goExercise,
+    goLogin,
+    goSignup,
+    goMyPage,
+  };
 };
