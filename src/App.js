e3f940e02c52bf62d5218ce7c17e86a3b6608b41
mewmaze <mewmaze@naver.com>
2026-02-16 14:42:50 +0900

    fix: 빌드 에러 import문 삭제

--- src/App.js
+++ src/App.js
@@ -6,109 +6,69 @@ import { ThemeProvider } from "@mui/material/styles";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import customTheme from "./theme/theme";
-import "./App.css";
 
 import Layout from "./layout/Layout";
-
-// MyPage 관련
 import MyPage from "./pages/mypage/MyPage";
 import Edit from "./pages/profile/ProfileEdit";
 import MyPost from "./pages/post/MyPost";
-
-// Auth 관련
 import Login from "./pages/login/Login";
 import Signup from "./pages/sigup/Signup";
-
-// Challenge 관련
 import Challenge from "./pages/challenge/Challenge";
 import Home from "./pages/Home";
 import ChallengeCreate from "./pages/challenge/ChallengeCreate";
 import ChallengeDetail from "./pages/challenge/ChallengeDetail";
 import ChallengeDiary from "./pages/challenge/ChallengeDiary";
-
-// Exercise 관련
 import Exercise from "./pages/exercise/Exercise";
 import ExerciseDiary from "./pages/exercise/ExerciseDiary";
-
-// Community 관련
 import CommunityList from "./pages/community/CommunityList";
 import Community from "./pages/community/Community";
-
-// Post 관련
 import PostDetail from "./pages/post/PostDetail";
 import NewPostPage from "./pages/post/NewPostPage";
 
-// 챌린지 Context 생성
-export const ChallengeStateContext = React.createContext();
 export const ChallengeDispatchContext = React.createContext();
 
-// 리듀서 함수 정의
 function reducer(state, action) {
   switch (action.type) {
-    case "INIT_CHALLENGE": {
+    case "INIT_CHALLENGE":
       return action.data;
-    }
-    case "CREATE_CHALLENGE": {
+    case "CREATE_CHALLENGE":
       return [...state, action.data];
-    }
-    case "UPDATE_CHALLENGE": {
+    case "UPDATE_CHALLENGE":
       return state.map((it) =>
         String(it.challenge_id) === String(action.data.challenge_id)
           ? { ...action.data }
-          : it
+          : it,
       );
-    }
-    case "DELETE_CHALLENGE": {
-      return state.filter((it) => String(it.id) !== String(action.id));
-    }
-    case "JOIN_CHALLENGE": {
+    case "DELETE_CHALLENGE":
+      return state.filter(
+        (it) => String(it.challenge_id) !== String(action.id),
+      );
+    case "JOIN_CHALLENGE":
       return state.map((it) =>
         it.challenge_id === parseInt(action.data.challenge_id, 10)
           ? {
               ...it,
-              participant_count: (parseInt(it.participant_count, 10) || 0) + 1, // 참가자 수 증가
+              participant_count: (parseInt(it.participant_count, 10) || 0) + 1,
             }
-          : it
+          : it,
       );
-    }
-    default: {
+    default:
       return state;
-    }
   }
 }
-const theme = customTheme;
+
 const App = () => {
   const [communities, setCommunities] = useState([
-    {
-      id: 1,
-      name: "런닝 게시판",
-      posts: [],
-    },
-    {
-      id: 2,
-      name: "자전거 게시판",
-      posts: [],
-    },
-    {
-      id: 3,
-      name: "헬스 게시판",
-      posts: [],
-    },
-    {
-      id: 4,
-      name: "다이어트 게시판",
-      posts: [],
-    },
-    {
-      id: 5,
-      name: "자유 게시판",
-      posts: [],
-    },
+    { id: 1, name: "런닝 게시판", posts: [] },
+    { id: 2, name: "자전거 게시판", posts: [] },
+    { id: 3, name: "헬스 게시판", posts: [] },
+    { id: 4, name: "다이어트 게시판", posts: [] },
+    { id: 5, name: "자유 게시판", posts: [] },
   ]);
 
   const addPost = (communityId, title, content) => {
-    setCommunities((prevCommunities) => {
-      return prevCommunities.map((community) => {
+    setCommunities((prev) =>
+      prev.map((community) => {
         if (community.id === parseInt(communityId)) {
           const newPost = {
             id: community.posts.length + 1,
@@ -119,13 +79,13 @@ const App = () => {
           return { ...community, posts: [...community.posts, newPost] };
         }
         return community;
-      });
-    });
+      }),
+    );
   };
 
   const addComment = (communityId, postId, text) => {
-    setCommunities((prevCommunities) => {
-      return prevCommunities.map((community) => {
+    setCommunities((prev) =>
+      prev.map((community) => {
         if (community.id === parseInt(communityId)) {
           const updatedPosts = community.posts.map((post) => {
             if (post.id === parseInt(postId)) {
@@ -137,113 +97,84 @@ const App = () => {
           return { ...community, posts: updatedPosts };
         }
         return community;
-      });
-    });
-  }; // 게시판 부분 끝
+      }),
+    );
+  };
 
-  //챌린지
-  const [challenges, dispatch] = useReducer(reducer, []);
+  const [, dispatch] = useReducer(reducer, []);
 
-  //서버에서 초기 데이터 가져오기
   useEffect(() => {
     const fetchChallenge = async () => {
       try {
         const response = await api.get("/challenges");
-        console.log("Fetched Data: ", response.data);
         dispatch({ type: "INIT_CHALLENGE", data: response.data });
       } catch (error) {
         console.error("Failed to fetch challenges:", error);
       }
     };
-
     fetchChallenge();
   }, []);
 
-  //goal은 props로 전달
-  const [goal] = useState({
-    height: "",
-    weight: "",
-    BMI: "",
-  }); // 챌린지, 나의 운동 기록 부분 끝
+  const [goal] = useState({ height: "", weight: "", BMI: "" });
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={customTheme}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <AuthContextProvider>
-          <ChallengeStateContext.Provider value={challenges}>
-            <ChallengeDispatchContext.Provider value={dispatch}>
-              <BrowserRouter>
-                <div className="App">
-                  <Layout>
-                    <Routes>
-                      <Route path="/" element={<Home />} />
-                      <Route path="/challenge" element={<Challenge />} />
-                      <Route
-                        path="/challengeDetail/:id"
-                        element={<ChallengeDetail />}
+          <ChallengeDispatchContext.Provider value={dispatch}>
+            <BrowserRouter>
+              <Layout>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/challenge" element={<Challenge />} />
+                  <Route
+                    path="/challengeDetail/:id"
+                    element={<ChallengeDetail />}
+                  />
+                  <Route
+                    path="/challengecreate"
+                    element={<ChallengeCreate />}
+                  />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signUp" element={<Signup />} />
+                  <Route path="/myPage/:user_id" element={<MyPage />} />
+                  <Route path="/myPage/:user_id/edit" element={<Edit />} />
+                  <Route
+                    path="/myPosts/:user_id/:postId"
+                    element={<MyPost />}
+                  />
+                  <Route path="/exercise" element={<Exercise goal={goal} />} />
+                  <Route path="/exercisediary" element={<ExerciseDiary />} />
+                  <Route path="/challengediary" element={<ChallengeDiary />} />
+                  <Route
+                    path="/communities"
+                    element={<CommunityList communities={communities} />}
+                  />
+                  <Route
+                    path="/community/:communityId"
+                    element={<Community communities={communities} />}
+                  />
+                  <Route
+                    path="/community/:communityId/new-post"
+                    element={<NewPostPage addPost={addPost} />}
+                  />
+                  <Route
+                    path="/community/:communityId/post/:postId"
+                    element={
+                      <PostDetail
+                        communities={communities}
+                        addComment={addComment}
                       />
-                      <Route
-                        path="/challengecreate"
-                        element={<ChallengeCreate />}
-                      />
-                      <Route path="/login" element={<Login />} />
-                      <Route path="/signUp" element={<Signup />} />
-                      <Route
-                        path="/myPage/:user_id"
-                        element={<MyPage />}
-                      />{" "}
-                      {/*  ... */}
-                      <Route
-                        path="/myPage/:user_id/edit"
-                        element={<Edit />}
-                      />{" "}
-                      {/*  ... */}
-                      <Route
-                        path="/myPosts/:user_id/:postId"
-                        element={<MyPost />}
-                      />
-                      <Route
-                        path="/exercise"
-                        element={<Exercise goal={goal} />}
-                      />
-                      <Route
-                        path="/exercisediary"
-                        element={<ExerciseDiary />}
-                      />
-                      <Route
-                        path="/challengediary"
-                        element={<ChallengeDiary />}
-                      />
-                      <Route
-                        path="/communities"
-                        element={<CommunityList communities={communities} />}
-                      />
-                      <Route
-                        path="/community/:communityId"
-                        element={<Community communities={communities} />}
-                      />
-                      <Route
-                        path="/community/:communityId/new-post"
-                        element={<NewPostPage addPost={addPost} />}
-                      />
-                      <Route
-                        path="/community/:communityId/post/:postId"
-                        element={
-                          <PostDetail
-                            communities={communities}
-                            addComment={addComment}
-                          />
-                        }
-                      />
-                    </Routes>
-                  </Layout>
-                </div>
-              </BrowserRouter>
-            </ChallengeDispatchContext.Provider>
-          </ChallengeStateContext.Provider>
+                    }
+                  />
+                </Routes>
+              </Layout>
+            </BrowserRouter>
+          </ChallengeDispatchContext.Provider>
         </AuthContextProvider>
       </LocalizationProvider>
     </ThemeProvider>
   );
 };
+
 export default App;
